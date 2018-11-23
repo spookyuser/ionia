17f35dbd223d29e118f39c9ae2e468ee8094686e
spookyuser <spookyUnknownUser@users.noreply.github.com>
2018-11-23 14:19:38 +0200

    Drop axes_cache

--- ionia/cache.py
+++ ionia/cache.py
@@ -59,8 +59,5 @@ class Cache:
             }
         except:
             return {
-                "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"},
-                "AXES_CACHE": {
-                    "BACKEND": "django.core.cache.backends.dummy.DummyCache"
-                },
+                "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}
             }
