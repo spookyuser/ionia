f9c5bddcd9524a4c27c5ec16b785a388f8a0d96d
spookyuser <spookyUnknownUser@users.noreply.github.com>
2018-11-27 20:52:54 +0200

    Add and refactor meta tests

--- ionia/tests/test_models.py
+++ ionia/tests/test_models.py
@@ -1,13 +1,19 @@
 import datetime
-import pytest
 
-from ..models import CommonInfo
+import pytest
+from django.db import IntegrityError
+
+from ..models import CommonInfo, get_id
 from .conftest import CommonInfoImplementation
 
 
 @pytest.mark.django_db
 class TestCommonInfo:
-    def test_generates_id(self):
+    def test_generates_id_in_abstract(self):
+        common_info_id = get_id()
+        assert common_info_id
+
+    def test_generates_id_in_model(self):
         common_info = CommonInfoImplementation.objects.first()
         assert common_info.id
 
@@ -16,12 +22,17 @@ class TestCommonInfo:
         second = CommonInfoImplementation.objects.all()[1]
         assert first.id < second.id
 
-    def test_ordering(self):
+    def test_id_is_unique(self):
+        common_info = CommonInfoImplementation.objects.create()
+        with pytest.raises(IntegrityError):
+            CommonInfoImplementation.objects.create(id=common_info.id)
+
+    def test_order_by_reverse_id(self):
         ordering = CommonInfo._meta.ordering
         print(ordering)
         assert ordering[0] == "-id"
 
-    def test_created_at(self):
+    def test_created_at_is_date(self):
         now = datetime.datetime.now()
         common_info = CommonInfoImplementation.objects.first()
         delta = now - common_info.created_at()
