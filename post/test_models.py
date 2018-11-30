b43c6ff08820d3f85b8dd6ffac2e92daf8b85f89
spookyuser <spookyUnknownUser@users.noreply.github.com>
2018-11-30 11:40:25 +0200

    Add meta tests to Post

--- post/test_models.py
+++ post/test_models.py
@@ -2,13 +2,36 @@ from django.db import models
 import pytest
 
 from .models import Post
-from island.models import Island
-from user.models import User
 
 
 @pytest.mark.django_db
 class TestPost:
-    def test_string(self):
-        # post = Post.objects.first()
-        # assert post.__str__() == post.post
-        User.objects.first()
+
+    # post = models.CharField(max_length=240)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # island = models.ForeignKey(Island, on_delete=models.CASCADE)
+
+    def test_post_label(self):
+        post = Post.objects.first()
+        field_label = post._meta.get_field("post").verbose_name
+        assert field_label == "post"
+
+    def test_user_label(self):
+        post = Post.objects.first()
+        field_label = post._meta.get_field("user").verbose_name
+        assert field_label == "user"
+
+    def test_island_label(self):
+        post = Post.objects.first()
+        field_label = post._meta.get_field("island").verbose_name
+        assert field_label == "island"
+
+    def test_post_max_length(self):
+        post = Post.objects.first()
+        field_max_length = post._meta.get_field("post").max_length
+        assert field_max_length == 240
+
+    def test_post_string(self):
+        post = Post.objects.first()
+        assert post.__str__() == post.post
+
